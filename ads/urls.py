a17cd05bab7e099df26f59fb54a1a20b2d208d5d
Atra-Fartash <atrafartash@gmail.com>
2025-09-19 17:13:31 +0330

    file completion

--- ads/urls.py
+++ ads/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from ads.views import (
     all_ads,
     all_categories,
-    delete_ads, City
+    delete_ads,
+    City
     )
 
 urlpatterns = [
-    path('ads', all_ads),
-    path('all-categories', all_categories),
-    path('delete-ad/<int:id>', delete_ads),
-    path('city/<slug:city>', City),
+    path('ads/', all_ads),
+    path('categories/<str:category_name>/', all_categories),
+    path('delete-ad/<int:ad_id>/', delete_ads),
+    path('<str:city_name>/', City),
 ]
--- ads/views.py
+++ ads/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404
 from ads.models import Category, Ad
 
 def all_ads(request):
@@ -6,16 +7,18 @@ def all_ads(request):
     ads = list(ads)
     return JsonResponse(ads, safe=False)
 
-def all_categories(request):
-    categories = Category.objects.all().values('name')
-    categories = list(categories)
-    return JsonResponse(categories, safe=False)
-
-def City(request, city):
-    ads = Ad.objects.filter(city = city).values('title')
+def all_categories(request, category_name):
+    category = get_object_or_404(Category,title__iexact = category_name)
+    ads = Ad.objects.filter(category_id =category.id).values('title')
     ads = list(ads)
     return JsonResponse(ads, safe=False)
 
-def delete_ads(request, id):
-    Ad.objects.get(id = id).delete()
+def City(request, city_name):
+    ads = Ad.objects.filter(city__name__iexact = city_name).values('title', 'price', 'area')
+    ads = list(ads)
+    return JsonResponse(ads, safe=False)
+
+def delete_ads(request, ad_id):
+    ads = get_object_or_404(Ad, id = ad_id)
+    ads.delete()
     return HttpResponse("Product Deleted")
